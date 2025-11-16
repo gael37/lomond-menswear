a1c4eba09b2a5db99d0a1c4d1b19df4c5857b30f
gael37 <gx.duchesne@gmail.com>
2025-11-16 03:54:26 +0000

    remove lib from git ignore

---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Lomond Menswear'
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Modern eccomerce urban wear store'
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
