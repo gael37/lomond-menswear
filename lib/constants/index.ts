3ad54012079329da5ebc6eed48bdabb48c24aca7
gael37 <gx.duchesne@gmail.com>
2025-11-19 07:32:24 +0000

    add sign up form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,11 +1,21 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Lomond Menswear'
-export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Modern eccomerce urban wear store'
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Lomond Menswear";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "Modern eccomerce urban wear store";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-email: 'jane@example.com',
-password: '123456'
-}
+  email: "jane@example.com",
+  password: "123456",
+};
+
+export const signUpDefaultValues = {
+  name: "Steve Smith",
+  email: "steve@example.com",
+  password: "123456",
+  confirmPassword: "123456",
+};
