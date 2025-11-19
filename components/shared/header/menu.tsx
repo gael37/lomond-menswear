9e85d94595d10c08d8965224148ee05a3e90c3e3
gael37 <gx.duchesne@gmail.com>
2025-11-19 06:22:26 +0000

    add dynamic dropdown when sign in

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,48 +1,44 @@
-import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { Sheet, SheetContent, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
-import Link from 'next/link';
-import ModeToggle from './mode-toggle';
+import { EllipsisVertical, ShoppingCart } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Link from "next/link";
+import ModeToggle from "./mode-toggle";
+import UserButton from "./user-button";
 
 const Menu = () => {
   return (
     <>
-      <div className='flex justify-end gap-3'>
-        <nav className='md:flex hidden w-full max-w-xs gap-1'>
+      <div className="flex justify-end gap-3">
+        <nav className="md:flex hidden w-full max-w-xs gap-1">
           <ModeToggle />
-          <Button asChild variant='ghost'>
-            <Link href='/cart'>
+          <Button asChild variant="ghost">
+            <Link href="/cart">
               <ShoppingCart />
               Cart
             </Link>
           </Button>
-          <Button asChild>
-            <Link href='/sign-in'>
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
+          <UserButton />
         </nav>
-        <nav className='md:hidden'>
+        <nav className="md:hidden">
           <Sheet>
-            <SheetTrigger className='align-middle'>
+            <SheetTrigger className="align-middle">
               <EllipsisVertical />
             </SheetTrigger>
-            <SheetContent className='flex flex-col items-start'>
+            <SheetContent className="flex flex-col items-start">
               <SheetTitle>Menu</SheetTitle>
               <ModeToggle />
-              <Button asChild variant='ghost'>
-                <Link href='/cart'>
+              <Button asChild variant="ghost">
+                <Link href="/cart">
                   <ShoppingCart />
                   Cart
                 </Link>
               </Button>
-               <Button asChild>
-                <Link href='/sign-in'>
-                  <UserIcon />
-                  Sign In
-                </Link>
-              </Button>
+              <UserButton />
             </SheetContent>
           </Sheet>
         </nav>
